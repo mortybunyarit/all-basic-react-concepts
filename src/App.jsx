b18eb3c3f630e71303628602bdbe966adf02a2ab
CodePawsDev <isripen1991@outlook.com>
2024-02-17 08:59:14 +0700

    feat: add more feature and some bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+/*  เมื่อกดปุ่ม favorite จะขึ้นข้อความว่า 'กดถูกใจหนังสือเล่มนี้'
+    เมื่อกดปุ่ม Reserve จะขึ้นข้อความว่า 'จองหนังสือเล่มนี้'
+    เมื่อกดปุ่ม Checkout จะขึ้นข้อความว่า 'ยืมหนังสือเล่มนี้' และจำนวนสิทธิ์ในการยืมหนังสือจะลดลง 1 สิทธิ์ 
+    เช่น เดิมมีสิทธิ์ยืม 5 เล่ม จะเหลือยืมหนังสือเพิ่มได้อีกเพียง 4 เล่ม เนื่องจากถือว่ายืมเล่มนี้ไปแล้ว
+    กรณีที่สิทธิ์ในการยืมหนังสือเหลือ 0/5 จะยืมหนังสือไม่ได้อีก และจะมีข้อความแจ้งเตือน 
+*/
 
 export default function App() {
-  const [userAction, setUserAction] = useState(0);
-  const bookCheckout = 2;
+  const [userAction, setUserAction] = useState([]);
+  const bookCheckout = 5;
+
+  const handleCheckOut = () => {
+    if (bookCheckout) {
+      
+    } else {
+      alert('คุณยืมหนังสือครบ 5 เล่มแล้ว ไม่สามารถยืมเพิ่มได้ หากมีข้อสงสัยกรุณาติดต่อเจ้าหน้าที่');
+    }
+  }
 
   return (
     <main>
@@ -25,12 +38,12 @@ export default function App() {
           <button className="border border-orange-400 bg-orange-600 rounded-lg p-2 text-white mx-5">
             Reserve
           </button>
-          <button className="border border-green-400 bg-green-600 rounded-lg p-2 text-white">
+          <button onClick={handleCheckOut} className="border border-green-400 bg-green-600 rounded-lg p-2 text-white">
             Checkout
           </button>
         </div>
         <p className="text-lg font-semibold text-red-800">
-          คุณได้ทำการ: {userAction}
+          คุณได้: 
         </p>
         <p className="font-semibold mt-2">คุณเหลือสิทธิ์ในการยืมหนังสือ <span className="underline decoration-double text-sky-500">{bookCheckout}/5</span> เล่ม</p>
       </section>
