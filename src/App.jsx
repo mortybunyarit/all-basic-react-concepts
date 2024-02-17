398465f8fe36663e3478b2cfc0f59f2fd9fb2ee0
mortybunyarit <bunyarit.saelim@gmail.com>
2024-02-17 14:16:18 +0700

    feat:translate-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <div>app</div>;
+  const [translate, setTranslate] = useState("Greeting Message");
+
+  return (
+    <div>
+      <div>
+        <p>{translate}</p>
+      </div>
+      <div>
+        <button onClick={() => setTranslate("สวัสดี")}>สวัสดี!</button>
+      </div>
+      <div>
+        <button onClick={() => setTranslate("Hi!")}>Hi!</button>
+      </div>
+      <div>
+        <button onClick={() => setTranslate("你好!")}>你好!</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
