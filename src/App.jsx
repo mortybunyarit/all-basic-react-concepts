35bec884126f2b31f2a444c034967a2707248c40
CodePawsDev <isripen1991@outlook.com>
2024-02-18 16:04:57 +0700

    feat: add new useState and event handler exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,43 @@
-/*  เมื่อกดปุ่ม favorite จะขึ้นข้อความว่า 'กดถูกใจหนังสือเล่มนี้'
-    เมื่อกดปุ่ม Reserve จะขึ้นข้อความว่า 'จองหนังสือเล่มนี้'
-    เมื่อกดปุ่ม Checkout จะขึ้นข้อความว่า 'ยืมหนังสือเล่มนี้' และจำนวนสิทธิ์ในการยืมหนังสือจะลดลง 1 สิทธิ์ 
-    เช่น เดิมมีสิทธิ์ยืม 5 เล่ม จะเหลือยืมหนังสือเพิ่มได้อีกเพียง 4 เล่ม เนื่องจากถือว่ายืมเล่มนี้ไปแล้ว
-    กรณีที่สิทธิ์ในการยืมหนังสือเหลือ 0/5 จะยืมหนังสือไม่ได้อีก และจะมีข้อความแจ้งเตือน 
-*/
+/*
+    ให้แก้ไข code เพื่อให้ application ทำงานได้ถูกต้อง 
+    1.เมื่อ user (พนักงานแคชเชียร์) กดปุ่ม 5% ราคาที่ลูกค้าจะต้องชำระจะลดจาก 1000 บาท เหลือ 950 บาท 
+    โดย UI จะต้องแสดงราคาเต็ม 1000 บาทในบรรทัดแรก ในบรรทัด Discount % ต้องแสดง 5% และในบรรทัดที่สามต้องแสดงราคาหลักหักส่วนลดคือ 950 
+    2. สำหรับปุ่มอื่นๆ หลักการทำงานเดียวกัน ต่างแค่ % ที่ลูกค้าได้รับส่วนลด 
+ */
 
 export default function App() {
-  const [userAction, setUserAction] = useState([]);
-  const bookCheckout = 5;
-
-  const handleCheckOut = () => {
-    if (bookCheckout) {
-      
-    } else {
-      alert('คุณยืมหนังสือครบ 5 เล่มแล้ว ไม่สามารถยืมเพิ่มได้ หากมีข้อสงสัยกรุณาติดต่อเจ้าหน้าที่');
-    }
-  }
+  const discount = 0;
+  const priceAfterDiscount = 2000;
 
   return (
-    <main>
-      <section className="bg-sky-200 h-[60px]">
-        <h1 className="font-bold text-4xl text-orange-700 p-2">Morty Library</h1>
-      </section>
-      <section className="pt-10 flex justify-center flex-col items-center">
-        <img
-          src={`https://picsum.photos/id/237/200/300`}
-          alt="book-cover"
-          className="w-[250px]"
-        />
-        <h1 className="font-bold text-2xl mt-4">
-          The life of lovely black dog named Soli
-        </h1>
-        <div className="my-4">
-          <button className="border border-pink-400 bg-pink-500 rounded-lg p-2 text-white">
-            Favourite
-          </button>
-          <button className="border border-orange-400 bg-orange-600 rounded-lg p-2 text-white mx-5">
-            Reserve
-          </button>
-          <button onClick={handleCheckOut} className="border border-green-400 bg-green-600 rounded-lg p-2 text-white">
-            Checkout
-          </button>
-        </div>
-        <p className="text-lg font-semibold text-red-800">
-          คุณได้: 
-        </p>
-        <p className="font-semibold mt-2">คุณเหลือสิทธิ์ในการยืมหนังสือ <span className="underline decoration-double text-sky-500">{bookCheckout}/5</span> เล่ม</p>
-      </section>
-    </main>
+    <div className="flex justify-center items-center w-full h-screen flex-col">
+      <main className="border-4 border-solid border-green-800 rounded-xl w-[550px] h-[340px]">
+        <section className="bg-green-600 p-6">
+          <h1 className="font-extrabold text-4xl text-white text-center">
+            Simple Price Discount
+          </h1>
+        </section>
+        <section className="pl-8 text-2xl font-semibold mt-5">
+          <div className="flex pl-8 justify-start">
+            <p className="pr-[100px]">Price without Discount</p>
+            <p>1000</p>
+          </div>
+          <div className="flex pl-8 justify-start">
+            <p className="pr-[248px]">Discount % : </p>
+            <p>{discount}</p>
+          </div>
+          <div className="flex pl-8 justify-start">
+            <p className="pr-[163px]">Price after Discount: </p>
+            <p>{priceAfterDiscount}</p>
+          </div>
+        </section>
+        <section className="flex gap-8 justify-center mt-7 font-semibold text-lg">
+          <button className="p-2 bg-yellow-400 rounded-md w-[65px]">5%</button>
+          <button className="p-2 bg-green-400 rounded-md w-[65px]">10%</button>
+          <button className="p-2 bg-orange-400 rounded-md w-[65px]">20%</button>
+          <button className="p-2 bg-purple-400 rounded-md w-[65px]">50%</button>
+        </section>
+      </main>
+    </div>
   );
 }
